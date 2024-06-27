cd15a064564bbf57fe7bb2d3ecdb7261fda1c86e
Enzoriel <enzoknsb@outlook.com>
2024-06-27 13:28:22 -0300

    Header completo

--- org/src/App.js
+++ org/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./componentes/Header/Header";
 
@@ -6,13 +5,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola mundo con React</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Aprende React
-        </a>
-      </header>
     </div>
   );
 }
