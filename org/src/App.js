d9acee463250a9b38af9c1241b361c3f77f5efeb
EnzorielDev <enzorieldev@outlook.com>
2024-06-28 13:33:31 -0300

    Añadiendo fuentes y empezando el formulario

--- org/src/App.js
+++ org/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Formulario from "./componentes/Formulario/Formulario";
 import Header from "./componentes/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Formulario />
     </div>
   );
 }
---
+++ org/src/componentes/Formulario/Formulario.jsx
@@ -0,0 +1,13 @@
+import "./Formulario.css";
+
+const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Rellena el formulario para crear a un colaborador.</h2>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
