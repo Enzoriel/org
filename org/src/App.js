bb44e7daeb205cc83a739b7ef0316fbbd58e66b9
EnzorielDev <enzorieldev@outlook.com>
2024-06-30 22:04:04 -0300

    Utilizando useState de React para agregar estados

--- org/src/App.js
+++ org/src/App.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Formulario from "./componentes/Formulario/Formulario";
 import Header from "./componentes/Header/Header";
+import MiOrg from "./componentes/MiOrg";
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true);
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Formulario />
+      {/* {mostrarFormulario ? <Formulario /> : <></>} */}
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
